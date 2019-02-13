ffa79aed0ee77ad2fcae0ecff478486b2fddc416
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-13 18:33:23 +0100

    Implement add, get and size methods for SinglyLinkedList class Implement Node class with proper getNext, setNext and getData methods Implement tests for add and get methods Change java version and junit dependency in pom file

--- src/main/java/com/codecool/SinglyLinkedList.java
+++ src/main/java/com/codecool/SinglyLinkedList.java
@@ -1,4 +1,75 @@
 package com.codecool;
 
-public class SinglyLinkedList {
+
+public class SinglyLinkedList<T> {
+    private Node head;
+    private Node last;
+    private int length;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    public void add(T data) {
+        if (head == null) {
+            this.head = new Node(data);
+            this.length++;
+            return;
+        }
+        Node current = head;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(new Node(data));
+        this.length++;
+        last = current;
+    }
+
+    public T get(int index) {
+        if (this.length == 1) {
+            return (T) this.head.getData();
+        }
+        Node current = head;
+        for(int currentIndex = 0; currentIndex < this.length; currentIndex++) {
+            if (currentIndex == index) {
+                return (T) current.getData();
+            }
+            current = current.getNext();
+        }
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public void insert(T data, int index) {
+
+    }
+
+    public int size() {
+        return this.length;
+    }
+}
+
+class Node<T> {
+    private T data;
+    private Node pointer;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return this.pointer;
+    }
+
+    public void setNext(Node<T> pointer) {
+        this.pointer = pointer;
+    }
+
+    public T getData() {
+        return this.data;
+    }
 }
--- src/test/java/com/codecool/SinglyLinkedListTest.java
+++ src/test/java/com/codecool/SinglyLinkedListTest.java
@@ -1,7 +1,41 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class SinglyLinkedListTest {
+
+    @Test
+    public void testSinglyLinkedListCreation() {
+        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
+    }
+
+    @Test
+    public void checkIfUserCanAddAString() {
+        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.add("some string");
+        singlyLinkedList.add("another string");
+        assertEquals(2, singlyLinkedList.size());
+    }
+
+    @Test
+    public void checkIfUserCanGetAString() {
+        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
+        String someString = "some string";
+        singlyLinkedList.add(someString);
+        assertEquals(someString, singlyLinkedList.get(0));
+    }
+
+    @Test
+    public void checkIfUserCanGetAStringFromListBiggerThanOne() {
+        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
+        String someString = "some string";
+        singlyLinkedList.add("something");
+        singlyLinkedList.add("something else");
+        singlyLinkedList.add(someString);
+        assertEquals(someString, singlyLinkedList.get(2));
+    }
+
 
 }
