30adadf461fa46f519ba27105276346b277c314e
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-15 11:37:44 +0100

    Refactor tests for SinglyLinkedList

--- src/test/java/com/codecool/SinglyLinkedListTest.java
+++ src/test/java/com/codecool/SinglyLinkedListTest.java
@@ -1,89 +1,102 @@
 package com.codecool;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class SinglyLinkedListTest {
+    SinglyLinkedList<String> singlyLinkedList;
+    String stringOne = "some string";
+    String stringTwo = "another string";
+    String stringThree = "yet another string";
 
-    @Test
-    public void testSinglyLinkedListCreation() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
+    @BeforeEach
+    public void createSinglyLinkedListInstance() {
+        this.singlyLinkedList = new SinglyLinkedList<>();
     }
 
     @Test
-    public void checkIfUserCanAddAString() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add("some string");
-        singlyLinkedList.add("another string");
-        assertEquals(2, singlyLinkedList.size());
+    public void testSizeWhenAddingStrings() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+
+        assertEquals(2, this.singlyLinkedList.size());
     }
 
     @Test
-    public void checkIfUserCanAddIntegers() {
-        SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add(34);
-        singlyLinkedList.add(21);
-        assertEquals(2, singlyLinkedList.size());
+    public void testSizeWhenAddingIntegers() {
+        SinglyLinkedList<Integer> singlyLinkedIntegerList = new SinglyLinkedList<>();
+
+        singlyLinkedIntegerList.add(34);
+        singlyLinkedIntegerList.add(21);
+        singlyLinkedIntegerList.add(45);
+
+        assertEquals(3, singlyLinkedIntegerList.size());
     }
 
     @Test
-    public void checkIfUserCanGetAString() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        String someString = "some string";
-        singlyLinkedList.add(someString);
-        assertEquals(someString, singlyLinkedList.get(0));
+    public void testGettingAStringFromListWithOneValue() {
+
+        this.singlyLinkedList.add(this.stringOne);
+
+        assertEquals(this.stringOne, this.singlyLinkedList.get(0));
     }
 
     @Test
-    public void checkIfUserCanGetAStringFromListBiggerThanOne() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        String someString = "some string";
-        singlyLinkedList.add("something");
-        singlyLinkedList.add("something else");
-        singlyLinkedList.add(someString);
-        assertEquals(someString, singlyLinkedList.get(2));
+    public void testGettingAStringFromListBiggerThanOne() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+        this.singlyLinkedList.add(this.stringThree);
+
+        assertEquals(this.stringThree, singlyLinkedList.get(2));
     }
 
     @Test
-    public void checkRemovingItemFromListHead() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add("something");
-        singlyLinkedList.add("something else");
-        singlyLinkedList.remove(0);
-        assertEquals("something else", singlyLinkedList.get(0));
+    public void testRemovingItemFromListHead() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+        this.singlyLinkedList.remove(0);
+
+        assertEquals(this.stringTwo, this.singlyLinkedList.get(0));
     }
 
     @Test
-    public void checkRemovingItemFromListTail() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add("something");
-        singlyLinkedList.add("something else");
-        singlyLinkedList.add("something else else");
-        singlyLinkedList.remove(1);
-        assertEquals("something else else", singlyLinkedList.get(1));
+    public void testRemovingItemFromListTail() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+        this.singlyLinkedList.add(this.stringThree);
+        this.singlyLinkedList.remove(1);
+
+        assertEquals(this.stringThree, this.singlyLinkedList.get(1));
     }
 
     @Test
-    public void checkInsertingDataToHead() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add("something");
-        singlyLinkedList.add("something else");
-        singlyLinkedList.add("something else else");
-        singlyLinkedList.insert("blahblah", 0);
-        assertEquals("blahblah", singlyLinkedList.get(0));
-        assertEquals(4, singlyLinkedList.size());
+    public void testInsertingDataToHead() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+        this.singlyLinkedList.add(this.stringThree);
+        this.singlyLinkedList.insert(this.stringThree, 0);
+
+        assertEquals(this.stringThree, this.singlyLinkedList.get(0));
+        assertEquals(4, this.singlyLinkedList.size());
     }
 
     @Test
-    public void checkInsertingDataToTail() {
-        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList<>();
-        singlyLinkedList.add("something");
-        singlyLinkedList.add("something else");
-        singlyLinkedList.add("something else else");
-        singlyLinkedList.insert("blahblah", 2);
-        assertEquals("blahblah", singlyLinkedList.get(2));
-        assertEquals(4, singlyLinkedList.size());
+    public void testInsertingDataToTail() {
+
+        this.singlyLinkedList.add(this.stringOne);
+        this.singlyLinkedList.add(this.stringTwo);
+        this.singlyLinkedList.add(this.stringThree);
+        this.singlyLinkedList.insert(this.stringOne, 2);
+
+        assertEquals(this.stringOne, this.singlyLinkedList.get(2));
+        assertEquals(4, this.singlyLinkedList.size());
     }
 
 
